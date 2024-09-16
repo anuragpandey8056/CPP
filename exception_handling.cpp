bdd9cf6b3bbef56d9d27a6d36fa1f2f663470dfc
Anurag pandey <anuragpandey8056@gmail.com>
2024-09-16 17:57:19 +0530

    change thread and exceptional handling file

--- exception_handling.cpp
+++ exception_handling.cpp
@@ -1,73 +1,74 @@
 
 //======================================================Exception handling=============================
+// Exceotion : Abnormal condition 
 // to avoid abnormal situation we use exceptional handling
 // there are three types of handler
 // 1).TRY
 // 2).THROW
 // 3).CATCH
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-// int a,b;
-// try{
-//     cout<<"enter two number \n";
-//     cin>>a>>b;
-//     if(b==0){
-//         throw b;
-//     }
-//     int c= a/b;
-//     cout<<c;
-// }
-// catch(int e){
-// cout<<"denominator cant be zero\n";
-
-// }
-// cout<<"done";
-// return 0 ;
-// }
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    int b;
-    cout << "enter a number\n";
-    cin >> b;
-    try
-    {
-        if (b == 0)
-        {
-            throw b;
-        }
-        if (b == -1)
-        {
-            throw 1.2;
-        }
-        if (b == -2)
-        {
-            throw 'c';
-        }
+int a,b;
+try{
+    cout<<"enter two number \n";
+    cin>>a>>b;
+    if(b==0){
+        throw b;
     }
-    catch (int d)
-    {
-        cout << "it is  a integer";
-    }
-    catch (double p)
-    {
-        cout << "it is  a float";
-    }
-    catch (char k)
-    {
-        cout << "it is  a character";
-    }
-    catch (...)
-    {
-        cout << "server down\n";
-    }
-
-    cout<<"done";
-
-    return 0;
+    int c= a/b;
+    cout<<c;
 }
+catch(int e){
+cout<<"denominator cant be zero\n";
+
+}
+cout<<"done";
+return 0 ;
+}
+//======================================================================================
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int b;
+//     cout << "enter a number\n";
+//     cin >> b;
+//     try
+//     {
+//         if (b == 0)
+//         {
+//             throw b;
+//         }
+//         if (b == -1)
+//         {
+//             throw 1.2;
+//         }
+//         if (b == -2)
+//         {
+//             throw 'c';
+//         }
+//     }
+//     catch (int d)
+//     {
+//         cout << "it is  a integer";
+//     }
+//     catch (double p)
+//     {
+//         cout << "it is  a float";
+//     }
+//     catch (char k)
+//     {
+//         cout << "it is  a character";
+//     }
+//     catch (...)
+//     {
+//         cout << "server down\n";
+//     }
+
+//     cout<<"done";
+
+//     return 0;
+// }
--- thread.cpp
+++ thread.cpp
@@ -2,35 +2,35 @@
 // // main is called a thread 
 
 
-// #include<iostream>
-// #include<thread>
+#include<iostream>
+#include<thread>
 
-// using namespace std;
-// void even (int start , int last ){
-//     for(int start ; start<= last ; start++){
-//         if(start%2==0){
-//             cout<<"even ="<<start<<"\t";
-//         }
-//     }
-// }
+using namespace std;
+void even (int start , int last ){
+    for(int start ; start<= last ; start++){
+        if(start%2==0){
+            cout<<"even ="<<start<<"\t";
+        }
+    }
+}
 
 
 
-// void odd (int start , int last ){
-//     for(int start ; start<= last ; start++){
-//         if(start%2!=0){
-//             cout<<"odd ="<<start<<"\t";
-//         }
-//     }
-// }
-// int main()
-// {
-// thread t1(even,1,1000);
-// cout<<"\n*********************************************************\n";
-// thread t2(odd,1,1000);
-// t1.join();
-// t2.join();
-// cout<<"\n byee byee \n";
+void odd (int start , int last ){
+    for(int start ; start<= last ; start++){
+        if(start%2!=0){
+            cout<<"odd ="<<start<<"\t";
+        }
+    }
+}
+int main()
+{
+thread t1(even,1,1000);
+cout<<"\n*********************************************************\n";
+thread t2(odd,1,1000);
+t1.join();
+t2.join();
+cout<<"\n byee byee \n";
 
-// return 0 ;
-// }
+return 0 ;
+}
